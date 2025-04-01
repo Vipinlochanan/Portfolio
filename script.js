2ce660bc0638c90911fb861f9abac257a3fc92e5
Vipin Lochanan <vipinvl48@gmail.com>
2025-04-01 22:15:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+		$(document).ready(function()
+		{
+			$(".skil1").mouseover(function()
+			{
+				$(".html").css("background-image", "url(./images/html.png)");
+				
+			});
+			$(".skil1").mouseout(function()
+			{
+				$(".html").css("background-image", "");
+			});
+
+            $(".skil2").mouseover(function()
+			{
+				$(".css").css("background-image", "url(./images/css.png)");
+			});
+			$(".skil2").mouseout(function()
+			{
+				$(".css").css("background-image", "");
+			});
+
+			$(".skil3").mouseover(function()
+			{
+				$(".java").css("background-image", "url(./images/java.png)");
+			});
+			$(".skil3").mouseout(function()
+			{
+				$(".java").css("background-image", "");
+			});
+
+			$(".skil4").mouseover(function()
+			{
+				$(".jquery").css("background-image", "url(./images/jquery.png)");
+			});
+			$(".skil4").mouseout(function()
+			{
+				$(".jquery").css("background-image", "");
+			});
+
+			$(".skil5").mouseover(function()
+			{
+				$(".boot").css("background-image", "url(./images/bootstrap.png)");
+			});
+			$(".skil5").mouseout(function()
+			{
+				$(".boot").css("background-image", "");
+			});
+
+			$(".skil6").mouseover(function()
+			{
+				$(".git").css("background-image", "url(./images/github.png)");
+			});
+			$(".skil6").mouseout(function()
+			{
+				$(".git").css("background-image", "");
+			});
+
+			$(".skil7").mouseover(function()
+			{
+				$(".python").css("background-image","url(./images/python.png)");
+			});
+			$(".skil7").mouseout(function()
+			{
+				$(".python").css("background-image", "");
+			});
+
+			$(".skil8").mouseover(function()
+			{
+				$(".django").css("background-image", "url(./images/django.png)");
+			});
+			$(".skil8").mouseout(function()
+			{
+				$(".django").css("background-image", "");
+			});
+		});
+		
+
+	
+
+
+
